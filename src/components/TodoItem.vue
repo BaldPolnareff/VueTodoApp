b7f7711991e34c235d851059519f7134bd8d958c
Giorgio <jeanpierrepolnareff@protonmail.com>
2023-07-05 12:22:30 +0200

    back to how the project was before, it worked locally and it's probably just an issue with how netlify does not build with certain errors

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-// import { defineProps, defineEmits } from 'vue';
+import { defineProps, defineEmits } from 'vue';
 import type { Todo } from '@/types/Todo';
 import { Icon } from '@iconify/vue'; 
 
